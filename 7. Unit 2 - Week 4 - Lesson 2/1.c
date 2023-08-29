94aeb9e037c98e2498714a383bad884fd80f0c52
HEMANTHKUMARANNAM <118728584+HEMANTHKUMARANNAM@users.noreply.github.com>
2023-08-29 21:14:51 +0530

    Added new files

---
+++ 7. Unit 2 - Week 4 - Lesson 2/1.c
@@ -0,0 +1,56 @@
+
+struct node
+{
+    int data;
+    struct node *next; 
+}; 
+typedef struct node *NODE;
+
+NODE createNode()
+{
+    NODE temp;
+    temp = (NODE) malloc(sizeof(struct node));
+    temp->next= NULL;
+    return temp;
+
+}
+
+NODE insertAtBegin(NODE first, int x) 
+{
+
+    NODE temp;
+    temp = createNode(); 
+    temp->data = x; 
+    NODE siv = first;
+    first = temp;
+    first->next = siv;
+    return first;
+}
+
+int count(NODE first)
+{
+    NODE temp; 
+    temp = first; 
+    int count =0; 
+    while(temp = NULL)
+    {
+        temp = temp->next; 
+        count++;
+    }
+
+    return count;
+}
+    
+void traverseList(NODE first) 
+{ 
+    NODE temp = first;
+
+    while (temp != NULL) 
+    {
+        printf("%d --> ",temp -> data); 
+        temp = temp ->next;
+
+    } 
+    printf("NULL\n");
+
+}
